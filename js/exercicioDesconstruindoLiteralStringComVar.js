79d0c1f387e82e8bcf4a13d5285e4f923a107e62
giorgiocost <gegiorgio1991@gmail.com>
2018-10-18 17:29:24 -0300

    Desafio para Resolver

---
+++ js/exercicioDesconstruindoLiteralStringComVar.js
@@ -0,0 +1,10 @@
+function addEndereco(endereco) {
+    const novoEndereco = {
+        cidade:endereco.cidade,
+        estado:endereco.estado,
+        pais: "Brasil"
+    }
+    console.log(novoEndereco.cidade+","+novoEndereco.estado+""+novoEndereco.pais);
+}
+
+addEndereco({cidade:"Campina Grande", estado:"Paraíba"});
