ad10cf608c98877c8c1bb5401451c2ee51455c55
giorgiocost <gegiorgio1991@gmail.com>
2018-10-19 12:24:49 -0300

    Código modular com import e export

---
+++ js/modularImportExport.js
@@ -0,0 +1,17 @@
+export const nome = "Giorgio";
+
+export function somar(x,y) {
+    return x+y;
+}
+
+export function dividir(x,y) {
+    return x/y;
+}
+
+export function subtrair(x,y) {
+    return x-y;
+}
+
+function multiplicar(x,y) {
+    return x*y;
+}
---
+++ js/moduleScript.js
@@ -0,0 +1,5 @@
+import { somar, nome } from './modularImportExport.js';
+
+console.log(somar(2,4));
+
+console.log(nome);
